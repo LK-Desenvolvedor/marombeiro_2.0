ab299c814e7b218077206734dee666b502ad5f2d
LK-Desenvolvedor <164733437+LK-Desenvolvedor@users.noreply.github.com>
2024-04-28 22:10:39 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,15 @@
-require("dotenv").config();
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const checkToken = require("./middleware/checkToken");
 
-
 const app = express();
 
 app.use(express.json());
-app.use("/user", checkToken);
-app.use("/", (req, res) => {
+app.get("/", (req, res) => { // Rota padrão
   res.status(200).json({ msg: "Bem vindo" });
 });
 app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
+app.use("/user", checkToken, userRoutes); // Protegendo todas as rotas de user
 
 module.exports = app;

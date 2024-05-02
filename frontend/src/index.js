55038e40a3a14e852e6bc22d1e912b1601a6ba07
LK-Desenvolvedor <164733437+LK-Desenvolvedor@users.noreply.github.com>
2024-05-02 01:02:30 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import CRUD from './CRUD';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CRUD/>
     <App />
   </React.StrictMode>
 );

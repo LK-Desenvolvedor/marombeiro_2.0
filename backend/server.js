f0199c978c706e579c4365dd428dd9a013119fa5
LK-Desenvolvedor <164733437+LK-Desenvolvedor@users.noreply.github.com>
2024-05-02 01:04:39 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const checkToken = require("./middleware/checkToken");
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
-app.get("/", (req, res) => { // Rota padrão
+app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem vindo" });
 });
 app.use("/auth", authRoutes);
-app.use("/user", checkToken, userRoutes); // Protegendo todas as rotas de user
+app.use("/user", checkToken, userRoutes);
 
 module.exports = app;
